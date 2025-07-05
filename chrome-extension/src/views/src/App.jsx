b59b21a07094db86c18cbdeb19f2c626d99329b5
burak behlül <burakyabgu@gmail.com>
2025-07-05 19:28:00 +0300

    feat: Add token fetcher action

--- chrome-extension/src/views/src/App.jsx
+++ chrome-extension/src/views/src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from 'react'
+
+import "./index.css"
+
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    chrome.storage.local.get('token', (result) => {
+      if (result.token) {
+        const token = result.token
+        const tokenParser = token.replace(/"/g, '')
+        setToken(tokenParser)
+      } else {
+        setToken(null)
+      }
+    })
+  })
   
   return (
 	<>
